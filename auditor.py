1b3a699c4447b058097d38923aa682d58236b1dd
s-brez <sam@sdbgroup.io>
2019-09-18 23:45:21 +1000

    Update auditor.py
    TX scrape working.

--- auditor.py
+++ auditor.py
@@ -1,4 +1,6 @@
 from web3 import Web3
+import requests
+import json
 
 
 class Auditor():
@@ -16,9 +18,36 @@ class Auditor():
 
     def scrape_transactions(self, address):
         """ Return a list of Publisher-formattted signal strings scraped from
-        a the given Ethereum public address. If the address's transactions
-        contain no signal strings, return False."""
-        pass
+        the given Ethereum address. If the address's transactions contain no
+        signal strings, return False.
+
+        We use the etherscan API to grab all transactions for an address as the
+        web3 python API lacks this capability - you'd have to manually iterate
+        through all previous blocks data to do this with web3, very slow."""
+
+        payload = (
+            "http://api-ropsten.etherscan.io/api?module=account&action=" +
+            "txlist&address=" + address + "&startblock=0&endblock=99999999&" +
+            "sort=asc&apikey=" + self.token)
+
+        # Poll Etherscan API
+        response = requests.get(payload).json()
+
+        # Strip only the transaction msg, convert it from hex string to bytes.
+        result = [
+            bytes.fromhex(i['input'][2:]) for i in response['result'] if self.check_hex(i['input'])]  # noqa
+
+        # Remove duplicates
+        result = list(dict.fromkeys(result))
+
+        print(result)
+
+    def check_hex(self, hex_data):
+        """Return true if the given hex string has len() > 2 and contains the
+        publisher header."""
+
+        if len(hex_data) > 2:
+            return True
 
     def parse_signals(self, signals: list):
         """Retunr a list of human-readable strings, given a list of Publisher
