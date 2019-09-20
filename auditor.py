ae173dc4f2507372988c71bf25a4c8bccfde9f1a
s-brez <sam@sdbgroup.io>
2019-09-20 16:16:11 +1000

    Update auditor.py
    - Add requirement for bytestring to have len(11)

--- auditor.py
+++ auditor.py
@@ -8,10 +8,11 @@ class Auditor():
     transaction data matching Publisher signal formatting, as well as parsing
     and displaying the signals in a human-readable format."""
 
-    def __init__(self, endpoint, data, etherscan_api_token):
+    def __init__(self, endpoint, data, etherscan_api_token, live: bool):
         self.endpoint = endpoint
         self.data = data
         self.token = etherscan_api_token
+        self.live = live
 
         # Connect to ETH node.
         self.w3 = Web3(Web3.HTTPProvider(self.endpoint))
@@ -22,34 +23,41 @@ class Auditor():
         signal strings, return False.
 
         We use the etherscan API to grab all transactions for an address as the
-        web3 python API lacks this capability - you'd have to manually iterate
-        through all previous blocks data to do this with web3, very slow."""
+        web3 python API lacks this capability - you'd have to manually parse
+        all previous blocks data to do this with web3, very slow."""
+
+        if self.live:
+            baseurl = "api"
+        else:
+            baseurl = "api-ropsten"
 
         payload = (
-            "http://api-ropsten.etherscan.io/api?module=account&action=" +
+            "http://" + baseurl + ".etherscan.io/api?module=account&action=" +
             "txlist&address=" + address + "&startblock=0&endblock=99999999&" +
             "sort=asc&apikey=" + self.token)
 
-        # Poll Etherscan API
+        # Get all transactions pertaining to the specified address
         response = requests.get(payload).json()
 
-        # Strip only the transaction msg, convert it from hex string to bytes.
+        # Strip transaction msg payloads, converting from hex string to bytes.
         result = [
-            bytes.fromhex(i['input'][2:]) for i in response['result'] if self.check_hex(i['input'])]  # noqa
+            {i['timeStamp']: bytes.fromhex(
+                i['input'][2:]).decode()} for i in response['result'] if self.verify(i, address)]  # noqa
 
-        # Remove duplicates
-        result = list(dict.fromkeys(result))
+        # Remove duplicates by converting list to set and back; return the list
+        return [dict(t) for t in {tuple(d.items()) for d in result}]
 
-        print(result)
+    def verify(self, tx, address):
+        """Return true if the given tx message begins with publisher header
+        "SAE", originated from the specified address and is len(11)."""
 
-    def check_hex(self, hex_data):
-        """Return true if the given hex string has len() > 2 and contains the
-        publisher header."""
-
-        if len(hex_data) > 2:
+        if(
+            (bytes.fromhex(tx['input'][2:]).decode()[:3] == "SAE") and
+                (tx['from'] == address.lower()) and (
+                    len(bytes.fromhex(tx['input'][2:]).decode()) == 11)):
             return True
 
     def parse_signals(self, signals: list):
-        """Retunr a list of human-readable strings, given a list of Publisher
-        -encoded signals strings."""
+        """Retun a list of human-readable strings, given a list of Publisher
+        -encoded signal strings."""
         pass
