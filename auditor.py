d2163f5a11ca55915b7966b9ac4a08ffdf500937
s-brez <sam@sdbgroup.io>
2019-09-21 10:56:10 +1000

    auditor.py finished

--- auditor.py
+++ auditor.py
@@ -1,4 +1,5 @@
 from web3 import Web3
+from datetime import datetime
 import requests
 import json
 
@@ -17,7 +18,7 @@ class Auditor():
         # Connect to ETH node.
         self.w3 = Web3(Web3.HTTPProvider(self.endpoint))
 
-    def scrape_transactions(self, address):
+    def scrape(self, address):
         """ Return a list of Publisher-formattted signal strings scraped from
         the given Ethereum address. If the address's transactions contain no
         signal strings, return False.
@@ -48,16 +49,49 @@ class Auditor():
         return [dict(t) for t in {tuple(d.items()) for d in result}]
 
     def verify(self, tx, address):
-        """Return true if the given tx message begins with publisher header
-        "SAE", originated from the specified address and is len(11)."""
+        """scrape() helper function. Return true if the given tx message begins
+        with publisher header "SAE", is len(11), and originated from address
+        parameter."""
 
         if(
             (bytes.fromhex(tx['input'][2:]).decode()[:3] == "SAE") and
+                # lower the address param as web3 returns lowercase addresses
                 (tx['from'] == address.lower()) and (
                     len(bytes.fromhex(tx['input'][2:]).decode()) == 11)):
             return True
 
-    def parse_signals(self, signals: list):
-        """Retun a list of human-readable strings, given a list of Publisher
-        -encoded signal strings."""
-        pass
+    def validate(self, param):
+        """Return True if param is either an address or private key."""
+
+        # check for address validity
+        if len(param) == 42 and param[:2] == "0x":
+            return "Valid"
+
+        # check for private key validity
+        if len(param) == 64 and type(int(param, 16)) == int:
+            return "Valid"
+        else:
+            return "Invalid"
+
+    def decode(self, signals: list):
+        """Return a list of human-readable signal strings, given a list of
+        Publisher-encoded signal dicts."""
+
+        decoded = []
+        if signals:
+            for sig in signals:
+                for key, val in sig.items():
+                    time = datetime.utcfromtimestamp(int(key))
+                    decoded.append([
+                            time.strftime("%H:%M:%S%z %d-%m-%Y"),
+                            self.data['data']['instrument'][val[3]],
+                            self.data['data']['inst_type'][val[4]],
+                            self.data['data']['exchange'][val[5]],
+                            self.data['data']['strategy'][val[6]],
+                            self.data['data']['direction'][val[7]],
+                            self.data['data']['order_type'][val[8]],
+                            self.data['data']['misc'][val[9]],
+                            self.data['data']['trigger'][val[10]]])
+            return decoded
+        else:
+            return None
